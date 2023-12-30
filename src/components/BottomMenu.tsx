38dcb2771601ebcabfbea9bbfc6f9974e4489962
anercan <anil.ercan@consultant.turkcell.com.tr>
2023-12-30 01:11:07 +0000

    fix bottom menu

--- src/components/BottomMenu.tsx
+++ src/components/BottomMenu.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, TouchableOpacity, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {useNavigation} from "@react-navigation/native";
 
 const BottomMenu = () => {
     let navigation = useNavigation();
+
     return (
         <View style={styles.bottomMenu}>
             <TouchableOpacity onPress={()=> navigation.navigate('QuizGroupListScreen')} style={styles.menuItem}>
-                <Icon name="home" size={30} color='#b2f6f6'/>
+                <Icon name={'align-left'} size={30} color='#b2f6f6'/>
             </TouchableOpacity>
             <TouchableOpacity onPress={()=> navigation.navigate('QuizGroupListScreen')} style={styles.menuItem}>
-                <Icon name="search" size={30} color='#b2f6f6'/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={()=> navigation.navigate('QuizGroupListScreen')} style={styles.menuItem}>
-                <Icon name="heart" size={30} color='#b2f6f6'/>
+                <Icon name="folder-o" size={30} color='#b2f6f6'/>
             </TouchableOpacity>
             <TouchableOpacity onPress={()=> navigation.navigate('SolvedQuizListScreen')} style={styles.menuItem}>
-                <Icon name="check-square" size={30} color='#b2f6f6'/>
+                <Icon name="user-o" size={30} color='#b2f6f6'/>
             </TouchableOpacity>
         </View>
     );
@@ -34,11 +32,13 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
         alignItems: 'center',
         backgroundColor: '#3e3939',
+        opacity:0.95,
         borderTopWidth: 1,
         borderTopColor: '#b3b3b3',
     },
     menuItem: {
         padding: 16,
+        paddingBottom:40
     },
 });
 
