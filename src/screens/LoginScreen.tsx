f58edb4144e5ddf65dc9c79e6f3de5a023049eaa
anercan <anil.ercan@consultant.turkcell.com.tr>
2024-09-04 01:15:54 +0100

    google authentication

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,62 +1,129 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import {
+    GoogleSignin, GoogleSigninButton,
+    isErrorWithCode,
+    isNoSavedCredentialFoundResponse,
+    isSuccessResponse,
+    statusCodes
+} from '@react-native-google-signin/google-signin';
+import apiCaller from "../config/apiCaller";
+import {IQuizGroupCard} from "../constants/types";
+
+function getGoogleConfig() {
+    return {
+        scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+        webClientId: '1017625843116-tj2gps7d9gsfd206psv7o76tqprekge2.apps.googleusercontent.com', // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
+    };
+}
 
 const LoginScreen = ({navigation}) => {
 
-  const [userInfo, setUserInfo] = useState(null);
+    const [state, setState] = useState<any>();
 
-  const signIn = async () => {
-    try {
-      /*await GoogleSignin.hasPlayServices();
-      const userInfo :any = await GoogleSignin.signIn();*/
-      setUserInfo(null);
-      console.log('User Info:', userInfo);
-      navigation.navigate('QuizGroupListScreen');
-    } catch (error) {
-      /*if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        Alert.alert('User cancelled the login flow');
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        Alert.alert('Sign in is in progress');
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        Alert.alert('Play services not available or outdated');
-      } else {
-        Alert.alert('An unknown error occurred', error.message);
-      }*/
-    }
-  };
+    useEffect(() => {
+        console.log('useeffect')
+        GoogleSignin.configure(getGoogleConfig());
+        hasPreviousSignIn();
+    }, []);
 
-  return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Life In The UK Preparation Kit</Text>
-        <Button title="Sign in with Google" onPress={signIn} />
-        {userInfo && (
+    const signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            console.log('evet')
+            const response:any = await GoogleSignin.signIn();
+            console.log('eve2t',response)
+            if (isSuccessResponse(response)) {
+                setState({userInfo: response.data});
+                console.log(response)
+                apiCaller('user-management/google-sign-in', 'POST', {token: response?.data?.idToken})
+                    .then(response => {
+                        console.log(response)
+                    });
+            } else {
+                // sign in was cancelled by user
+            }
+        } catch (error) {
+            if (isErrorWithCode(error)) {
+                switch (error.code) {
+                    case statusCodes.IN_PROGRESS:
+                        console.log('error',error)
+                        break;
+                    case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+                        console.log('error',error)
+                        break;
+                    default:
+                        console.log('error',error)
+                }
+            } else {
+                console.log('error',error)
+            }
+        }
+    };
+
+    const getCurrentUser = async () => {
+        try {
+            const response:any = await GoogleSignin.signInSilently();
+            if (isSuccessResponse(response)) {
+                console.log('getCurrentUser',response)
+                setState({ userInfo: response.data });
+                navigation.navigate('QuizGroupListScreen')
+            } else if (isNoSavedCredentialFoundResponse(response)) {
+                // user has not signed in yet
+            }
+        } catch (error) {
+            // handle errror
+        }
+    };
+
+    const hasPreviousSignIn = async () => {
+        const hasPreviousSignIn = GoogleSignin.hasPreviousSignIn();
+        console.log('hasPreviousSignIn',hasPreviousSignIn)
+        setState({ hasPreviousSignIn });
+        getCurrentUser();
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Life In The UK Test</Text>
+            <GoogleSigninButton style={styles.googleButton}
+                size={GoogleSigninButton.Size.Standard}
+                onPress={() => {
+                    signIn()
+                }}
+                disabled={false}
+            />
+            {state && (
             <View style={styles.userInfo}>
-              <Text style={styles.userInfoText}>Welcome, {userInfo.user.name}</Text>
-              <Text style={styles.userInfoText}>{userInfo.user.email}</Text>
+                <Text style={styles.userInfoText}>Welcome, {state?.userInfo?.name}</Text>
+                <Text style={styles.userInfoText}>{state?.userInfo?.email}</Text>
             </View>
         )}
-      </View>
-  );
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  userInfo: {
-    marginTop: 20,
-  },
-  userInfoText: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    userInfo: {
+        marginTop: 20,
+    },
+    userInfoText: {
+        fontSize: 16,
+        marginBottom: 8,
+    },
+    googleButton: {
+    },
+
 });
 
 export default LoginScreen;
